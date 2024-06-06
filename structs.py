03bbb7549fb7de80895888d31a9ae3b82d41c2a5
Jing Liu <jing.liu@duke.edu>
2024-06-06 15:47:40 -0400

    created struct objects for GRN

---
+++ structs.py
@@ -0,0 +1,17 @@
+class GeneNode:
+    def __init__(self, g, e):
+        '''
+        gene (string): name of gene
+        edges (list): list of Edges that this gene regulates
+        '''
+        self.gene = g
+        self.edges = e
+
+class Edge:
+    def __init__(self, t, s):
+        '''
+        target (GeneNode): the gene the Edge is pointing to/regulating
+        sign (int): 1 if Edge is activating, 0 if Edge is inhibiting
+        '''
+        self.target = t
+        self.sign = s
