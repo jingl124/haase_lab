8b2c55d490d403bb394fd2756839f9723ed247d4
Jing Liu <jing.liu@duke.edu>
2024-06-07 16:18:05 -0400

    using grn_finder.py functions in jupyter

--- grn_finder.py
+++ grn_finder.py
@@ -6,7 +6,10 @@ import matplotlib.pyplot as plt
 import glob
 import numpy as np
 
-def extract_expression_data(path, sep=',', strain=None):
+def dummy():
+    print("hi")
+
+def extract_expression_data(path, sep=','):
     '''
     This function extracts the necessary information from 
     path(string): includes location and name of file
@@ -14,15 +17,10 @@ def extract_expression_data(path, sep=',', strain=None):
     return: 
     '''
     df = pd.read_csv(path, sep=sep)
-    if strain is not None: 
-        df = df[df['strain'] == strain]
-    df = df[['TF', 'strain', 'GeneName', 'time', 'log2_shrunken_timecourses']]
-
-def extract_TF_data(df, tf):
-    '''
-    df (Pandas DataFrame): DataFrame containing the expression data of all TFs
-    tf (string)
-    '''
+    selected_columns = ['TF', 'strain', 'GeneName', 'time', 'log2_shrunken_timecourses']
+    df = df[selected_columns]
+    print("Expression data extracted.")
+    return df
 
 def get_t_fall():
     '''
@@ -34,6 +32,19 @@ def get_t_rise():
     return: list of floats
     '''
 
-def build_tree():
+def build_tree(tf, df, threshold):
     '''
+    Create a tree structure with a height of 1 for a given TF.
+    tf (string): TF
+    df (DataFrame): Pandas DataFrame with expression data
     '''
+    # df_tf = df[]
+
+def build_network(tfs):
+    '''
+    The main function for building the overall network.
+    tfs (list): list of strings representing TFs
+    '''
+    for tf in tfs:
+        build_tree(tf)
+    
